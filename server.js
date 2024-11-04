8d096133edb3e8b47cb4ca8cad377770accb2cae
DouglasDev222 <douglasdev222@gmail.com>
2024-11-04 18:11:43 -0300

    Adicionando Endpoints de Agendamentos

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const baseRoutes = require('./routes/baseRoutes');
 const client = require('./config/database');
+const agendamentosRouter = require('./routes/agendamentoRouter');
+
 
 // Carregar variáveis de ambiente do arquivo .env
 dotenv.config();
@@ -14,6 +16,7 @@ app.use(express.json());
 
 // Rotas
 app.use('/api', baseRoutes);
+app.use('/api', agendamentosRouter);
 
 // Testar conexão com o banco de dados PostgreSQL
 client.query('SELECT NOW()', (err, res) => {
